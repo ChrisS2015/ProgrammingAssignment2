f48809e7e322fbfac4f23d068d0fe16bfda598f9
ChrisS2015 <snevets_cm@yahoo.co.uk>
2015-03-21 16:51:46 +0000

    Adding completed function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## these functions will cache the results of the matrix solve function
+## in order to save time if a previously calculated solve
+## is re-requested
 
-## Write a short comment describing this function
+## this function creates the cache object
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function determines whether the cache contains the solution
+## and retireves the result from the cache if present
+## otherwise it will calculate the solution and cache that result
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    if(!is.null(m)) {
+        message("Getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
 }
